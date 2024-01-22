aa3539c6f5f350cecd252dd42ad720757415af45
Victor <practicas1@drakharstudio.com>
2024-01-22 23:51:37 +0000

    Retoque.

--- Assets/_PROYECTO/SCRIPTS/GENERAL/ObjectPool.cs
+++ Assets/_PROYECTO/SCRIPTS/GENERAL/ObjectPool.cs
@@ -2,17 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPool : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+namespace VictorRivero{
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	/// <summary>
+	/// 
+	/// </summary>
+
+	public class ObjectPool : MonoBehaviour
+	{
+		#region Static Fields
+		#endregion
+		#region Const Field
+		#endregion
+		#region Param Fields
+		#endregion
+		#region Private Fields
+		#endregion
+		#region Public Fields
+		#endregion
+		#region Lifecycle
+		#endregion
+		#region Public API
+		#endregion
+		#region Unity Methods
+		// Start is called before the first frame update
+		void Start()
+		{
+			
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			
+		}
+
+		// Awake is called when the script is
+		// first loaded or when an object is
+		// attached to is instantiated
+		void Awake()
+		{
+			
+		}
+	    
+		// FixedUpdate is called at fixed time intervals
+		void FixedUpdate()
+		{
+			
+		}
+            
+		// LateUpdate is called after all Update functions have been called
+		#endregion
+		#region Private Methods
+		#endregion            
+		#region Public Methods
+		#endregion
+	}
 }
