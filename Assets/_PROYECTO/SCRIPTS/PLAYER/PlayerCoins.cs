37ebe6b4ef56bed114e9c431c39c747c5fc8579c
LeynaD0 <danielsmd521@gmail.com>
2024-01-23 09:17:27 +0000

    Particulas y Sonido de moneda

---
+++ Assets/_PROYECTO/SCRIPTS/PLAYER/PlayerCoins.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCoins : MonoBehaviour
+{
+    [SerializeField] private GameObject particlePrefab;
+
+    [SerializeField] private int points;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+            Destroy(collision.gameObject);
+        }
+
+        Instantiate(particlePrefab);
+
+        
+    }
+}
