8a1cb6c97f515772ac694b36f363dc970881ae2d
Tayri9 <tayrifernandez@gmail.com>
2024-01-23 12:57:43 +0000

    rotacion player

--- Assets/_PROYECTO/SCRIPTS/PLAYER/PlayerJump.cs
+++ Assets/_PROYECTO/SCRIPTS/PLAYER/PlayerJump.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class PlayerJump : MonoBehaviour
 {
     [SerializeField] float gravity;
+    [SerializeField] float speed;
+    [SerializeField] GameObject humo;
+    bool isRotating;
+    bool isTop;
+    bool isBot;
     private void Start()
     {
+        isRotating = false;
+        isTop = false;
+        isBot = true;
         gameObject.GetComponent<Rigidbody2D>().gravityScale = gravity;
     }
 
@@ -14,7 +23,37 @@ public class PlayerJump : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            Instantiate(humo, transform.position, transform.rotation);
             gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
+            //transform.Rotate(0, 0, 180);
+            isRotating = true;
+            isTop = !isTop;
+            isBot = !isBot;
+        }
+
+        if (isRotating)
+        {
+            if (isBot) 
+            {
+                Debug.Log("isBot");
+                transform.Rotate(0, 0, -speed * Time.deltaTime);
+                if (transform.rotation.eulerAngles.z <= 10)
+                {
+                    isRotating = false;
+                    transform.Rotate(0, 0, 0);                    
+                }
+            } 
+            else
+            {
+                Debug.Log("isTop");
+                transform.Rotate(0, 0, speed * Time.deltaTime);
+                if (transform.rotation.eulerAngles.z >= 180)
+                {
+                    isRotating = false;
+                    transform.Rotate(0, 0, 0);                    
+                }
+            }
+            
         }
     }
 }
