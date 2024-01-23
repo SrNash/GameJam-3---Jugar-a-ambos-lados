c4ed12919e3c8329e4666597cbdfc1fb64e6382e
LeynaD0 <danielsmd521@gmail.com>
2024-01-23 10:16:24 +0000

    Menu sound

--- Assets/_PROYECTO/SCRIPTS/PLAYER/PlayerCoins.cs
+++ Assets/_PROYECTO/SCRIPTS/PLAYER/PlayerCoins.cs
@@ -6,8 +6,6 @@ public class PlayerCoins : MonoBehaviour
 {
     [SerializeField] private GameObject particlePrefab;
 
-    [SerializeField] private int points;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Coin"))
